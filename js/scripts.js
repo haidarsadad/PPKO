934dc941fbad4aaa69183167aa515e742aa82e67
Haidar Sadad <haidarsadad@MacBook-Air-Haidar.local>
2026-02-23 11:16:02 +0700

    update website

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
-
-window.addEventListener('scroll',()=>{
- const nav=document.querySelector('.nav');
- if(nav) nav.style.background=window.scrollY>50?'#000':'rgba(0,0,0,.3)';
+// navbar effect
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    if (nav) nav.style.background = window.scrollY > 50 ? '#000' : 'rgba(0,0,0,.3)';
 });
+
+// detail product tabs
+function openDetailTab(id, el) {
+    document.querySelectorAll('.detail-content').forEach(c => {
+        c.classList.remove('active');
+    });
+
+    document.querySelectorAll('.detail-tab').forEach(b => {
+        b.classList.remove('active');
+    });
+
+    const target = document.getElementById(id);
+    if (target) {
+        target.classList.add('active');
+    }
+
+    el.classList.add('active');
+}
